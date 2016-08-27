25445216130451341993a1c8bf9db25f458c934e
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-08-27 21:19:00 +0200

    omnibox hinting finished and styled

--- dist/fancy-search.js
+++ dist/fancy-search.js
@@ -7,7 +7,7 @@
       // scope: {
       //   search: "="
       // },
-      link: function ($scope) {
+      link: function ($scope, $element) {
         $scope.hints = ["AAA", "AAB", "AAC", "ABA", "CCC", "CCA", "CCB"]; // TODO: sort data at start;
         $scope.isActive = false;
         $scope.search = {
@@ -15,13 +15,14 @@
           postfix: "",
           hint: ""
         };
-        
+
         $scope.updateQuery = function () {
               for (var i in $scope.hints) {
                 if($scope.hints[i].toUpperCase().lastIndexOf($scope.search.query.toUpperCase(), 0) === 0)
                   return $scope.search.hint = $scope.hints[i];
               } return $scope.search.hint = "";
             };
+
         $scope.choose = function (value) {
             return $scope.search = {
                   query: value,
@@ -29,6 +30,7 @@
                   hint: value
             };
         }
+
         $scope.$watch('search.query', function (newVal, oldVal) {
             var res = ($scope.search.query) ?
                           $scope.search.hint.substr($scope.search.query.length) : "";
@@ -36,6 +38,13 @@
             return $scope.search.postfix = res;
         });
 
+        $element.bind("keydown keypress", function(event) {
+            var keyCode = event.which || event.keyCode;
+            if (keyCode === 9) {
+              $scope.updateQuery();
+              $scope.choose($scope.search.hint);
+            }
+        });
 
         // return console.log(scope)
       },
