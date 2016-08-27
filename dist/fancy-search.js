3a71aaf9765588de475e562913fc57b347741418
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-08-27 19:22:22 +0200

    styled hinting mockup

--- dist/fancy-search.js
+++ dist/fancy-search.js
@@ -4,14 +4,33 @@
   app.directive('fancysearch',  function() {
     return {
       restrict: 'E',
-      scope: true,
-      link: function (scope) {
-        scope.search = {};
-        scope.hints = ["Hint AAA", "Hint AAB", "Hint AAC", "Hint AA", "Hint CCC", "Hint CCA", "Hint CCB"];
-        scope.isActive = false;
-        scope.hint = "test"+scope.search.query;
+      // scope: {
+      //   search: "="
+      // },
+      link: function ($scope) {
+        $scope.search = {};
+        $scope.search.query = "";
+        $scope.search.postfix = "";
+        $scope.search.hint = "";
 
-        // return console.log("")
+        $scope.hints = ["Hint AAA", "Hint AAB", "Hint AAC", "Hint AA", "Hint CCC", "Hint CCA", "Hint CCB"];
+        $scope.isActive = false;
+        $scope.updateQuery = function () {
+              for (var i in $scope.hints) {
+                if($scope.hints[i].toUpperCase().lastIndexOf($scope.search.query.toUpperCase(), 0) === 0)
+                  return $scope.search.hint = $scope.hints[i];
+              } return "";
+            };
+
+        $scope.$watch('search.query', function (newVal, oldVal) {
+            var res = ($scope.search.query) ?
+                          $scope.search.hint.substr($scope.search.query.length) : "";
+
+            return $scope.search.postfix = res;
+        });
+
+
+        // return console.log(scope)
       },
       templateUrl: 'partials/fancy-search.html'
     };
