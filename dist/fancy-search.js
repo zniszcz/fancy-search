2742bd9763a31d36f3158a5755c56fd4d2ee0d17
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-08-27 19:54:04 +0200

    little refactor: search model separated

--- dist/fancy-search.js
+++ dist/fancy-search.js
@@ -8,20 +8,27 @@
       //   search: "="
       // },
       link: function ($scope) {
-        $scope.search = {};
-        $scope.search.query = "";
-        $scope.search.postfix = "";
-        $scope.search.hint = "";
-
         $scope.hints = ["AAA", "AAB", "AAC", "ABA", "CCC", "CCA", "CCB"]; // TODO: sort data at start;
         $scope.isActive = false;
+        $scope.search = {
+          query: "",
+          postfix: "",
+          hint: ""
+        };
+        
         $scope.updateQuery = function () {
               for (var i in $scope.hints) {
                 if($scope.hints[i].toUpperCase().lastIndexOf($scope.search.query.toUpperCase(), 0) === 0)
                   return $scope.search.hint = $scope.hints[i];
               } return $scope.search.hint = "";
             };
-
+        $scope.choose = function (value) {
+            return $scope.search = {
+                  query: value,
+                  postfix: "",
+                  hint: value
+            };
+        }
         $scope.$watch('search.query', function (newVal, oldVal) {
             var res = ($scope.search.query) ?
                           $scope.search.hint.substr($scope.search.query.length) : "";
