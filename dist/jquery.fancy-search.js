8d5bc4245fbb8bf230b550dd6f7641ad8373a0e1
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-08-28 20:07:41 +0200

    omnibox working

--- dist/jquery.fancy-search.js
+++ dist/jquery.fancy-search.js
@@ -7,15 +7,24 @@
       config = {
         placeholder: "Search...",
         submit: "Go!",
-        hintNumbers: 3
+        hintNumbers: 3,
+        hintURL: "https://zniszcz.github.io/fancy-search/feed/hints.json"
       }
 
     var form = $('form.fancy-search'),
-        raw = "<div class='input-group'><div class='currentHint'><span class='query'></span><span class='hint'></span></div><input class='input' type='text' placeholder='"+config.placeholder+"'/><input class='button' type='submit' value='"+config.submit+"' /><ul class='hints'> </ul></div>";
+        hints = [],
+        raw = "<div class='input-group'><div class='currentHint'><span class='query'></span><span class='hint'></span></div><input name='q' class='input' type='text' autocomplete='off' placeholder='"+config.placeholder+"'/><input class='button' type='submit' value='"+config.submit+"' /><ul class='hints'> </ul></div>";
 
         // TODO:
         // 1. Uwzględnić tylko kilka parametrów przekazanych w configu
         // 2.
+
+        $.ajax({
+          url: config.hintURL,
+          success: function (data) {
+            hints = data.hints;
+          }
+        });
 
         form.html(raw).ready( function () {
           var search = {
@@ -24,7 +33,7 @@
               },
               input = form.find('.input'),
               button = form.find('.button'),
-              hints = form.find('.hints');
+              hintsList = form.find('.hints');
 
               input.focus(function () {
                   form.toggleClass('active');
@@ -33,11 +42,41 @@
                   form.toggleClass('active');
                 })
                 .on('input', function () {
-                  var val = $(this).val();
-                  search.query.html(val);
-                });
+                  var val = $(this).val(),
+                      firstHint = getFirstHint(val),
+                      postfix = (firstHint && val) ? firstHint.substr(val.length) : "";
 
+                  search.query.html(val);
+                  search.hint.html(postfix);
+
+                  $(this).keydown( function (e) {
+                      var code = e.keyCode || e.which,
+                          pass = true;
+
+                      if(code == '9') {
+                        e.preventDefault();
+
+                        search.query.html(firstHint);
+                        search.hint.html("");
+
+                        input.val(firstHint);
+                        pass = !pass;
+                      }
+                      return pass;
+                  });
+                });
         });
+
+        function getFirstHint(val) {
+          var filtred = $.grep(hints, function (phrase) {
+              return phrase.toUpperCase().search(val.toUpperCase()) == 0;
+          })
+          return filtred[0];
+        }
+
+        function filtreHints () {
+
+        }
   }
 
 })(jQuery);
